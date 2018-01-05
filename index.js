ab2bce45b89dc8350b835ab133984c4a7287d0cc
Erkan Demirkol <Erkan Demirkol>
2018-01-05 20:20:07 +0300

    make it better

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const prefix = "/";
+
+bot.login('Mzk3MDU5ODk2MDUyNTQ3NTk3.DTBrXw.E7YIMydUb1d3dJSfult9ApdEu6c');
+
+bot.on("ready",function(){
+console.log('Bot Çalışıyor : '+ bot.user.tag)
+});
+
+bot.on("message", function(msg){
+if(msg.content === prefix + "test")
+{
+msg.channel.send('Başarılı Dostum !');
+}
+});
+
+
+
+
 app.get("/",function(req,res)
 {
     res.send("Merhaba deneme");
 });
+
 app.listen(port);
