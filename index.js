5fcfd57d42f05cf5077d42f8cbaee943f2f7e514
Erkan Demirkol <Erkan Demirkol>
2018-01-05 21:35:43 +0300

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { isContext } from "vm";
+
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
@@ -5,28 +7,31 @@ var port = process.env.PORT || 3000;
 
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const prefix = "/";
 
-bot.login('Mzk3MDU5ODk2MDUyNTQ3NTk3.DTBrXw.E7YIMydUb1d3dJSfult9ApdEu6c');
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("./Configuration/config.json", "utf8"));
 
-bot.on("ready",function(){
-console.log('Bot Çalışıyor : '+ bot.user.tag)
+
+bot.login(config.token);
+
+bot.on("ready", function () {
+    console.log('Bot Çalışıyor : ' + bot.user.tag)
 });
 
-bot.on("message", function(msg){
-if(msg.content === prefix + "test")
-{
-msg.channel.send('Başarılı Dostum !');
-}
-});
 
-bot.on("message", function(msg){
-    if(msg.content === prefix + "yardım")
-    {
-    msg.channel.send('Komut Listesi : /test , /yardım ,/temizle (Mesaj Sayısı)');
+bot.on("message", function (msg) {
+
+    if (msg.content === config.prefix + "yardım") {
+        msg.channel.send('Komut Listesi : /test , /yardım ,/temizle (Mesaj Sayısı)');
     }
-    });
-    
+    if (msg.content === config.prefix + "test") {
+        msg.channel.send('Başarılı Dostum !');
+    }
+    if (msg.content === config.prefix + "zarsalla") {
+        var x = Math.floor((Math.random() * 6) + 1);
+        msg.channel.send(x);
+    }
+});
 
 
 
@@ -40,8 +45,7 @@ bot.on("message", function(msg){
 
 
 
-app.get("/",function(req,res)
-{
+app.get("/", function (req, res) {
     res.send("Merhaba deneme");
 });
 
